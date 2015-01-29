ca0d9fbcca808c4c3e2535ba113a3d0ee2c1a306
pparm <pauli.parmanne@gmail.com>
2015-01-29 16:15:20 +0200

    eka testi

---
+++ Haritmetiikka/src/main/java/haritmetiikka/haritmetiikka/lukuArpoja.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package haritmetiikka.haritmetiikka;
+import java.util.Random;
+/**
+ *
+ * @author poplinus
+ */
+
+public class lukuArpoja {
+
+    public int luku(){
+  int START = 1;
+  int END = 10;
+  
+  Random arpoja = new Random();
+return 2;
+    
+} 
+}
---
+++ Haritmetiikka/src/test/java/haritmetiikka/haritmetiikka/lukuArpojaTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package haritmetiikka.haritmetiikka;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author poplinus
+ */
+public class lukuArpojaTest {
+    
+    public lukuArpojaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+//     TODO add test methods here.
+//     The methods must be annotated with annotation @Test. For example:
+//    
+//     @Test
+     public void hello() {}
+
+@Test
+    public void lukuArvotaanOikein(){
+ lukuArpoja luku = new lukuArpoja();
+assertEquals(2,luku.luku());
+    
+}
+}
