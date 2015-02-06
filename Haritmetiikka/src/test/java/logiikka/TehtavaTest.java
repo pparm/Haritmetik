17966ca35bbfac2d4839b29c91b900c22afab55f
pparm <pauli.parmanne@gmail.com>
2015-02-06 02:05:41 +0200

    kolmas palautus

---
+++ Haritmetiikka/src/test/java/logiikka/TehtavaTest.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logiikka;
+import haritmetiikka.logiikka.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author poplinus
+ */
+public class TehtavaTest {
+    Tehtava tehtava;
+    public TehtavaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    
+    
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    
+    tehtava = new Tehtava();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+     public void tehtavaTest1() {
+     assertEquals(true,tehtava.getOikein("summa",10,10,20));
+     
+     }
+
+    @Test
+     public void tehtavaTest2() {
+     assertEquals(false,tehtava.getOikein("summa",10,10,30));
+     
+     }
+    @Test
+     public void tehtavaTest3() {
+     assertEquals(true,tehtava.getOikein("summa",0,0,0));
+     
+     }
+     
+    @Test
+     public void tehtavaTest4() {
+     assertEquals(false,tehtava.getOikein("summa",-2,0,0));
+     
+     }
+    @Test
+     public void tehtavaTest5() {
+     assertEquals(true,tehtava.getOikein("summa",-2,-2,-4));
+     
+     }
+    @Test
+     public void tehtavaTest6() {
+     assertEquals(-4,tehtava.getOikeaVastaus("summa",-2,-2));
+     
+     }
+    @Test
+     public void tehtavaTest7() {
+     assertEquals(0,tehtava.getOikeaVastaus("summa",0,0));
+     
+     }
+    @Test
+     public void tehtavaTest8() {
+     assertEquals(100,tehtava.getOikeaVastaus("summa",50,50));
+     
+     }
+    @Test
+     public void tehtavaTest9() {
+     assertEquals(1,tehtava.getOikeaVastaus("summa",100,-99));
+     
+     }
+}
